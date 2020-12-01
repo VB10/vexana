1f36ccd067e9645f8ad148634047cf719656335e
Veli Bacik <veli.bacik@ibtech.com.tr>
2020-12-01 21:48:51 +0300

    prefrecnes and local file options added to export file

--- lib/vexana.dart
+++ lib/vexana.dart
@@ -12,6 +12,10 @@ export 'src/model/error_model.dart';
 export 'src/model/response_model.dart';
 export 'src/model/enum/request_type.dart';
 
+// NETWORK
 export 'src/network_manager.dart';
-
 export 'package:dio/src/options.dart';
+
+// CACHE
+export 'src/cache/file/local_file.dart';
+export 'src/cache/shared/local_preferences.dart';
