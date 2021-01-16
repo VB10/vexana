3bc119f6194363b2ea7de8762e44fdc217443302
Veli Bacik <veli.bacik@ibtech.com.tr>
2021-01-16 14:50:50 +0300

    rpimtive type to model

---
+++ test/dog/http_dog_test.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:vexana/vexana.dart';
+
+main() {
+  INetworkManager networkManager;
+  setUp(() {
+    networkManager = NetworkManager(isEnableLogger: true, options: BaseOptions(baseUrl: "https://hwasampleapi.firebaseio.com"));
+  });
+  test("Json Place Holder Todos", () async {
+    final response = await networkManager.fetch<EmptyModel, EmptyModel>("/dogs/0/code.json", parseModel: EmptyModel(), method: RequestType.GET);
+
+    expect(response.data, isList);
+  });
+}
