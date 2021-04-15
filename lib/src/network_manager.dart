8cd10953b0b3004e79e20bec8e5ac657026d2c6c
Veli Bacik <>
2021-04-15 12:29:45 +0300

    documentantion and intercaptors send

--- lib/src/network_manager.dart
+++ lib/src/network_manager.dart
@@ -20,12 +20,6 @@ import 'package:dio/src/dio.dart';
 import 'package:dio/src/dio_error.dart';
 import 'package:dio/src/interceptors/log.dart';
 import 'package:dio/src/cancel_token.dart';
-
-// import 'package:dio/dio.dart';
-
-// import 'package:dio/dio.dart';
-
-// import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 
@@ -45,16 +39,47 @@ part 'operation/network_cache.dart';
 part 'operation/network_model_parser.dart';
 part 'operation/network_wrapper.dart';
 
+/// Network manager provide your requests with [Dio]
+///
+/// Example:
+/// [NetworkManager(isEnableLogger: true, errorModel: UserErrorModel(),]
+/// [options: BaseOptions(baseUrl: "https://jsonplaceholder.typicode.com/"));]
 class NetworkManager with DioMixin implements Dio, INetworkManager {
+  /// [Future<DioError> Function(DioError error, NetworkManager newService)] of retry service request with new instance
+  ///
+  /// Default value function is null until to define your business.
   late Future<DioError> Function(DioError error, NetworkManager newService)? onRefreshToken;
+
+  /// [VoidCallback?] has send error if it has [onRefreshToken] callback after has problem.
+  ///
+  /// Default value function is null work with [onRefreshToken].
   late VoidCallback? onRefreshFail;
-  late final int maxCount = 3;
+
+  /// [int?] retry maxiumum count at refresh function.
+  late final int _maxCount = 3;
+
+  /// [int?] retry  count at per refresh call.
+  ///
+  /// This increment with [onRefreshToken]
   late int _retryCount;
+
+  /// [IFileManager?] manage cache operation with this.
+  ///
+  /// Example keep your data in json file:
+  /// [NetworkManager(fileManager: LocalFile())]
   late IFileManager? fileManager;
+
+  /// [INetworkModel?] is repsone model for every request.
+  ///
+  /// Example:
+  /// [LoginModel()]
   late INetworkModel? errorModel;
 
   /// [Client] has be set deafult client adapter
   bool isEnableTest;
+
+  /// [Interceptors] return dio client interceptors list
+  Interceptors get dioIntercaptors => interceptors;
 
   NetworkManager({
     required BaseOptions options,
@@ -72,9 +97,27 @@ class NetworkManager with DioMixin implements Dio, INetworkManager {
     //TODO: Http adapter has come
     httpClientAdapter = DefaultHttpClientAdapter();
   }
+
   void _addLoggerInterceptor(bool isEnableLogger) {
     if (isEnableLogger) interceptors.add(LogInterceptor());
   }
+
+  @override
+
+  /// [bool] clear all cache on network manager.
+  Future<bool> removeAllCache() async => await _removeAllCache();
+
+  /// [Future<IResponseModel<R?>> send<T extends INetworkModel, R>] will complete your request with paramaters
+  ///
+  /// [T parseModel] netwwork response parser model
+  /// [RequestType method] network request type like [RequestType.GET]
+  ///
+  /// Example:
+  /// ''
+  /// await networkManager.send<Todo, List<Todo>>("/todosPost",
+  ///  parseModel: Todo(), method: RequestType.POST, data: todoPostRequestBody);
+  ///
+  /// '''
 
   @override
   Future<IResponseModel<R?>> send<T extends INetworkModel, R>(
@@ -89,7 +132,7 @@ class NetworkManager with DioMixin implements Dio, INetworkManager {
     dynamic data,
     ProgressCallback? onReceiveProgress,
   }) async {
-    final cacheData = await getCacheData<R, T>(expiration, method, parseModel);
+    final cacheData = await _getCacheData<R, T>(expiration, method, parseModel);
     if (cacheData is ResponseModel<R>) {
       return cacheData;
     }
@@ -103,7 +146,7 @@ class NetworkManager with DioMixin implements Dio, INetworkManager {
       switch (response.statusCode) {
         case HttpStatus.ok:
           await writeCache(expiration, response.data, method);
-          return getResponseResult<T, R>(response.data, parseModel);
+          return _getResponseResult<T, R>(response.data, parseModel);
         default:
           return ResponseModel(error: ErrorModel(description: response.data.toString()));
       }
@@ -112,7 +155,7 @@ class NetworkManager with DioMixin implements Dio, INetworkManager {
     }
   }
 
-  Future<ResponseModel<R>?> getCacheData<R, T extends INetworkModel>(Duration? expiration, RequestType type, T responseModel) async {
+  Future<ResponseModel<R>?> _getCacheData<R, T extends INetworkModel>(Duration? expiration, RequestType type, T responseModel) async {
     // TODO: Web Cache support
     if (kIsWeb) return null;
     if (expiration == null) return null;
@@ -120,11 +163,11 @@ class NetworkManager with DioMixin implements Dio, INetworkManager {
     if (cacheDataString == null) {
       return null;
     } else {
-      return getResponseResult<T, R>(jsonDecode(cacheDataString), responseModel);
+      return _getResponseResult<T, R>(jsonDecode(cacheDataString), responseModel);
     }
   }
 
-  ResponseModel<R> getResponseResult<T extends INetworkModel, R>(dynamic data, T parserModel) {
+  ResponseModel<R> _getResponseResult<T extends INetworkModel, R>(dynamic data, T parserModel) {
     final model = _parseBody<R, T>(data, parserModel);
     return ResponseModel<R>(data: model);
   }
@@ -144,7 +187,4 @@ class NetworkManager with DioMixin implements Dio, INetworkManager {
     final _data = data is Map ? data : jsonDecode(data);
     error.model = errorModel!.fromJson(_data);
   }
-
-  @override
-  Future<bool> removeAllCache() async => await _removeAllCache();
 }
