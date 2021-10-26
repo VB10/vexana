96146ca4b706d2419c7ad4236dd19b02ef6bd046
kanieren <kanieren@gmail.com>
2021-10-27 00:39:58 +0300

    conflicts were resolved

--- example/lib/json_place_holder/json_place_holder.dart
+++ example/lib/json_place_holder/json_place_holder.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import './json_place_holder_view.dart';
 
 class JsonPlaceHolder extends StatefulWidget {
-  const JsonPlaceHolder({Key key}) : super(key: key);
+  const JsonPlaceHolder({Key? key}) : super(key: key);
 
   @override
   JsonPlaceHolderView createState() => JsonPlaceHolderView();
