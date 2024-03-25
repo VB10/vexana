a69bc0a987ac184a707aebc275066a9ddad5feba
Behzod <behzod.faiziev@gmail.com>
2024-03-25 03:21:02 +0300

    added consts and finals to classes

--- lib/src/interface/i_response_model.dart
+++ lib/src/interface/i_response_model.dart
@@ -6,11 +6,11 @@ import 'package:vexana/src/interface/index.dart';
 /// occurred while receiving the data.
 abstract class IResponseModel<T, E extends INetworkModel<E>?> {
   /// The `IResponseModel` constructor is used to create a new `IResponseModel`
-  IResponseModel(this.data, this.error);
+  const IResponseModel(this.data, this.error);
 
   /// [data] is a getter method that returns the data received from the server.
-  T data;
+  final T data;
 
   /// [error] is a getter method that returns any errors that occurred while
-  IErrorModel<E>? error;
+  final IErrorModel<E>? error;
 }
